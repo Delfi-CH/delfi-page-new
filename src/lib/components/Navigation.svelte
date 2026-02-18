17a5083c3cbd09c809fc48ce711b9a6b268c1e14
Delfi-CH <laspitzer@gmx.ch>
2026-02-18 11:22:44 +0100

    add github to Navigation.svelte

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -8,6 +8,7 @@
         {#each Routes as route (route.name)}
             <li class="navItem"><a class="navItemLink" href={resolve(route.href)}>{route.name}</a></li>
         {/each}
+        <li class="github"><a class="githubLink" href="https://github.delfi.dev">GitHub</a></li>
     </ul>
 </main>
 
@@ -23,7 +24,6 @@
         display: flex;
         flex-direction: row;
         justify-content: center;
-        flex-wrap: wrap;
     }
     .navItem {
         padding: 1rem 0;
@@ -41,13 +41,31 @@
         text-decoration: none;
     }
     .navItemLink:hover {
-        width: fit-content;
         background: #131416;
+        color: #34c777;
+    }
+
+    .github {
+        padding: 1rem 0;
+        margin: 0.5rem 0;
+        align-self: flex-end;
+    }
+
+    .githubLink {
+        width: fit-content;
+        background: #34c777;
+        border: 5px solid #34c777;
         border-radius: 2rem;
         margin: 0.7rem 0.5rem;
         padding: 0.3rem 0.7rem;
-        color: #34c777;
+        color: #1A2426;
         font-size: 130%;
         text-decoration: none;
+    }
+
+    .githubLink:hover {
+        background: #131416;
+        color: #34c777;
+
     }
 </style>
