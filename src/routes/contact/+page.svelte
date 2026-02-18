155a0f159b7a10940ff83b4662cb3a42d176bb62
Delfi-CH <laspitzer@gmx.ch>
2026-02-18 22:34:45 +0100

    add contact

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+
+import Navigation from "$lib/components/Navigation.svelte";
+import Footer from "$lib/components/Footer.svelte";
+</script>
+
+<main>
+    <Navigation></Navigation>
+    <h1>Contact me:</h1>
+    <p>Discord: @delfi1423</p>
+    <p>Email (private): delfi at delfi.dev</p>
+    <p>Email (buisness): buisness at delfi dot dev</p>
+    <h2>Social Media</h2>
+    <p>GitHub: @Delfi-CH</p>
+    <Footer></Footer>
+</main>
+
+<style>
+    h1, h2, p {
+        margin-left: 1em;
+    }
+</style>
