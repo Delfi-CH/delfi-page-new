be01bcbc1913b0e470510c1cc6f4564f0632b699
Delfi-CH <laspitzer@gmx.ch>
2026-02-18 12:24:44 +0100

    add the root page

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -1,5 +1,6 @@
 export const Routes =  [
     {name: "Home", href: "/"},
+    {name: "About me", href: "/about"},
     {name: "My Projects", href: "/projects"},
     {name: "Contact", href: "/contact"},
     {name: "Classic Page", href: "https://classic.delfi.dev/"},
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script>
 
 import Navigation from "$lib/components/Navigation.svelte";
+import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <main>
     <Navigation></Navigation>
-    <h1>Hello</h1>
+    <h1>hello, world</h1>
+    <p>Welcome to Delfi's personal website. This is a showcase of my software-projects and myself as a person.</p>
+    <p>Feel free to look around.</p>
+    <Footer></Footer>
 </main>
 
