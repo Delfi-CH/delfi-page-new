5f73ebc47fa38442867689d967aa16116466d158
Delfi-CH <laspitzer@gmx.ch>
2026-02-18 21:39:05 +0100

    add setup

---
+++ src/lib/setup.js
@@ -0,0 +1,110 @@
+class Device {
+    name;
+    manufacturer;
+    manufacturerUrl;
+    processor;
+    graphicsProcessor;
+    memory;
+    storage;
+    type;
+    usage;
+    other;
+
+    constructor(name, manufacturer, manufacturerUrl , processor, graphicsProcessor, memory, storage, type, usage, other) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.manufacturerUrl = manufacturerUrl;
+        this.processor = processor;
+        this.graphicsProcessor = graphicsProcessor;
+        this.memory = memory;
+        this.storage = storage;
+        this.type = type;
+        this.usage = usage;
+        this.other = other
+    }
+}
+
+const DeviceType = Object.freeze({
+    Desktop: "Desktop Computer",
+    Laptop: "Laptop-Computer",
+    Phone: "Mobile Phone",
+    Tablet: "Tablet-Computer",
+    Console: "Game Console",
+    Other: "Other"
+ })
+
+const DeviceUseCase = Object.freeze({
+    Gaming: "Gaming",
+    Development: "Software development",
+    Office: "Office work",
+    Phone: "Phone",
+    Entertainment: "Entertainment",
+    Server: "Server",
+})
+
+export const DeviceList = [
+    new Device(
+        "ThinkPad L13 2-in-1",
+        "Lenovo",
+        "https://www.lenovo.com/ch/de/p/laptops/thinkpad/thinkpadl/lenovo-thinkpad-l13-2-in-1-gen-5-13-inch-intel/len101t0100#models",
+        "Intel Core Ultra 7 265U",
+        "Intel HD Graphics",
+        "32GB 6400 MHz LPDDR5",
+        "1TB NVMe SSD",
+        DeviceType.Laptop,
+        [DeviceUseCase.Development, DeviceUseCase.Office],
+        "Convertable Laptop"
+    ),
+    new Device(
+        "Custom-Built Gaming PC",
+        "Myself",
+        null,
+        "AMD Ryzen 7 9800X3D",
+        "AMD Radeon Rx 9070 (Gigabyte OC) + AMD Radeon HD Graphics",
+        "32GB 5600MHz DDR5 RAM",
+        " 2TB NVMe SSD",
+        DeviceType.Desktop,
+        [DeviceUseCase.Development, DeviceUseCase.Entertainment, DeviceUseCase.Gaming],
+        "MSI PRO B850-P WIFI Motherboard"
+    ),
+    new Device(
+        "Mac Pro (Early 2009 Edition)",
+        "Apple",
+        "https://support.apple.com/en-us/112590",
+        "2x Intel Xeon X5550 at 2.66 GHz",
+        "AMD Radeon HD 8950 or Equivalent",
+        "56GB 1033 MHz DDR3 ECC RAM",
+        "512GB SATA SSD",
+        DeviceType.Desktop,
+        [DeviceUseCase.Server],
+        "DVD-RW SATA Drive"
+    ),
+    new Device(
+        "Fairphone 6",
+        "Fairphone",
+        "https://shop.fairphone.com/the-fairphone-gen-6",
+        "Snapdragon® 7s Gen 3 Processor",
+        "Snapdragon Adreno",
+        "8GB LPDDR5",
+        "256GB internal storage",
+        DeviceType.Phone,
+        [DeviceUseCase.Phone, DeviceUseCase.Entertainment],
+        "SD-Card Slot"
+    ),
+]
+export const Acessories = [
+    {type: "Mouse", name: "Logitech G502 Hero", url: "https://www.logitechg.com/en-us/products/gaming-mice/g502-hero-gaming-mouse.html?sp=4&searchclick=gaming"},
+    {type: "Keyboard", name: "Corsair K55 Pro Lite", url: "https://www.corsair.com/us/en/p/keyboards/ch-9226065-na/k55-rgb-pro-lite-gaming-keyboard-ch-9226065-na"},
+    {type: "Monitor", name: "ASUS TUF Gaming VG27AQMA", url: "https://www.asus.com/displays-desktops/monitors/tuf-gaming/tuf-gaming-vg27aqm1a/"},
+    {type: "Monitor", name: "Hannspree HL 162 Portable Monitor", url: "Hannspree HL 162 Portable Monitor"},
+    {type: "Headphones", name: "Logitech G733", url: "https://www.logitechg.com/de-ch/shop/p/g733-rgb-wireless-headset.981-000864?sp=1&searchclick=Logitech%20G"},
+    {type: "Gamepad", name: "Logitech F310 Gamepad", url: "https://www.logitech.com/de-ch/shop/p/f310-gamepad.940-000135?sp=1&searchclick=Logitech%20G"},
+    {type: "Gamepad", name: "Nintendo Switch Pro Controller", url: "https://www.nintendo.com/us/store/products/pro-controller/"}
+]
+
+export const OperatingSystems = [
+    "Bazzite Linux",
+    "Arch Linux (btw)",
+    "Rasberry Pi OS",
+    "Windows 11"
+]
---
+++ src/routes/about/setup/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+    /* eslint-disable svelte/no-navigation-without-resolve */
+    import Navigation from "$lib/components/Navigation.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+    import {DeviceList, OperatingSystems, Acessories} from "$lib/setup.js";
+
+</script>
+<main>
+    <Navigation></Navigation>
+    <h1>My Setup:</h1>
+    <h2>Devices:</h2>
+    <div class="deviceList">
+        {#each DeviceList as device (device.name)}
+            <h3>{device.name}</h3>
+            <p>{device.type}, used for/as: {device.usage.join(', ')}</p>
+            <p>Manufacturer: {device.manufacturer} {#if !!device.manufacturerUrl} <a href={device.manufacturerUrl} target="_blank">View official website</a> {/if}</p>
+            <p>CPU: {device.processor} | GPU: {device.graphicsProcessor} | RAM: {device.memory} | Storage: {device.storage} | {device.other}</p>
+        {/each}
+    </div>
+    <h2>Accessories</h2>
+    <div class="deviceList">
+        {#each Acessories as accessory (accessory.name)}
+            <h3>{accessory.type}</h3>
+            <p><a href={accessory.url} target="_blank">{accessory.name}</a></p>
+        {/each}
+    </div>
+    <h2>Operating Systems:</h2>
+    <p class="last">{OperatingSystems.join(', ')}</p>
+    <Footer></Footer>
+</main>
+<style>
+    h1, h2, .deviceList, .deviceList h3, p {
+        margin-left: 1em;
+    }
+    .last {
+        margin-left: 1em;
+        padding-left: 1em;
+        margin-bottom: 10%;
+    }
+    .deviceList {
+        padding-left: 1em;
+    }
+    a {
+        color: #34C777;
+    }
+</style>
