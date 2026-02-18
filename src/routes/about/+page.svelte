ef8168804f566bd68b22c2b7a3f77791bf69fbff
Delfi-CH <laspitzer@gmx.ch>
2026-02-18 12:56:02 +0100

    add /about

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+
+    import Navigation from "$lib/components/Navigation.svelte";
+    import {resolve} from "$app/paths";
+    import Footer from "$lib/components/Footer.svelte";
+
+</script>
+<main>
+    <Navigation></Navigation>
+    <h1>About me:</h1>
+    <p>Hello! I am Delfi</p>
+    <p>I am a Computer Science student from Switzerland</p>
+    <h2>My programming experience</h2>
+    <p>When I was younger, I had the desire to create my own video game. This eventually lead me to go study Computer Science after 9th grade, where I am now.</p>
+    <p>At school, I am currently learning full-stack web development, primarily using ReactJS for frontends, Java SpringBoot for backends with a MariaDB Database. </p>
+    <p>I also have some experience with Python (mostly for data visualisation with matplotlib) and in writing UNIX shell scripts with GNU BASH.</p>
+    <p>Durimg my free time, I have also started learning lower level languages like Rust and C, mostly for command-line applications.</p>
+    <h2>Linux and Open Source</h2>
+    <p>TODO</p>
+    <h2><a href={resolve("/about/setup")}>My Setup</a></h2>
+    <Footer></Footer>
+</main>
+<style>
+
+</style>
