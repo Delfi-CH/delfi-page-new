7a2c1883ce9b845e9ed02222f6cdb9266e57d95a
Delfi-CH <laspitzer@gmx.ch>
2026-02-19 09:11:04 +0100

    create deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+git fetch
+git checkout main
+git pull
+git checkout pages
+git pull
+git rebase main pages
+npm run build
+cp -r ./build ./docs
---
+++ docs/_app/immutable/entry/start.CiIRZxZw.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/D3R7VYXP.js";import{A as t}from"../chunks/CmBfMGLw.js";export{t as load_css,r as start};
