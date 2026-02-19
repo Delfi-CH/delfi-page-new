23a18e5d78f4b974ea6092a7f980d244171b378d
Delfi-CH <laspitzer@gmx.ch>
2026-02-19 13:39:16 +0100

    improve deploy.sh again again

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 git fetch
 git checkout main
-git pull --no-rebase
+git pull --rebase
 git checkout pages
-git pull --no-rebase
+git pull --rebase
 git rebase main pages
 npm run build
 cp -r ./build ./docs
