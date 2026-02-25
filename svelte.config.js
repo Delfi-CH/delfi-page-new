dca2593695850ccb563c42b52ea716b6ee1e96a6
Delfi-CH <laspitzer@gmx.ch>
2026-02-25 10:49:51 +0100

    Continious build

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,11 @@ export default {
 			base: ''
 		},
 		prerender: {
-			default: true
+			handleHttpError: ({ path, referrer, message }) => {
+				// ignore deliberate link to shiny 404 page
+				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') { return; }
+				throw new Error(message);// otherwise fail the build throw new Error(message);
+			}
 		}
 	}};
+
