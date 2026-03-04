440f312793be27140f1892c517158979153612d0
Delfi-CH <delfi@delfi.dev>
2026-03-04 19:07:11 +0100

    add 88x31

---
+++ src/lib/88x31.js
@@ -0,0 +1,39 @@
+const eightyeightXthirtyones = [
+    { filename: "ddg.gif", url: "https://duckduckgo.com" },
+    { filename: "debian-powered.gif", url: "https://debian.org" },
+    { filename: "got_html.gif", url: "https://www.w3.org/standards/history/html52/" },
+    { filename: "java_green_button.gif", url: "https://java.com" },
+    { filename: "js-warning.gif", url: "https://en.wikipedia.org/wiki/JavaScript#Security" },
+    { filename: "linux-p.gif", url: "https://distrowatch.com" },
+    { filename: "notepadpp3.gif", url: "https://notepad-plus-plus.org/" },
+    { filename: "poweredbyfedora.gif", url: "https://fedoraproject.org" },
+    { filename: "powered-llvm.gif", url: "https://llvm.org  " },
+    { filename: "right2repair.gif", url: "https://en.wikipedia.org/wiki/Right_to_repair" },
+    { filename: "steam.gif", url: "https://store.steampowered.com/" },
+    { filename: "tfs.gif", url: "https://www.gnu.org/" },
+    { filename: "valid-html5.gif", url: "https://www.w3.org/standards/history/html52/" },
+    { filename: "visitmini.gif", url: "https://distrowatch.com" },
+    { filename: "wii.gif", url: "https://wii.hacks.guide" },
+    { filename: "debian.gif", url: "https://debian.org" },
+    { filename: "fftake.gif", url: "https://librewolf.net/" },
+    { filename: "half-life.gif", url: "steam://run/70" },
+    { filename: "javascript.gif", url: "https://en.wikipedia.org/wiki/JavaScript#Security" },
+    { filename: "latex.gif", url: "https://www.latex-project.org/" },
+    { filename: "linux_powered.gif", url: "https://distrowatch.com" },
+    { filename: "powered-by-debian.gif", url: "https://debian.org" },
+    { filename: "powered-cpp.gif", url: "https://www.gnu.org/software/gcc/projects/cxx-status.html" },
+    { filename: "redhat.gif", url: "https://redhat.com" },
+    { filename: "sendit.gif", url: "https://delfi.dev" },
+    { filename: "sun.gif", url: "https://en.wikipedia.org/wiki/Sun_Microsystems" },
+    { filename: "valid-css-blue.gif", url: "https://www.w3.org/TR/css/#css" },
+    { filename: "vim_a.gif", url: "https://neovim.io" },
+    { filename: "warpnow.gif", url: "https://en.wikipedia.org/wiki/OS/2" },
+    { filename: "wikipedia.gif", url: "https://en.wikipedia.org" }
+];
+
+export function getRandom88x31() {
+    const rand = Math.floor(Math.random()*eightyeightXthirtyones.length)
+    console.log(rand)
+    console.log(eightyeightXthirtyones[rand])
+    return eightyeightXthirtyones[rand]
+}
--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,11 +1,21 @@
 <script>
-    const currentYear = new Date()
+    import {getRandom88x31} from "$lib/88x31.js";
+    import { onMount } from "svelte";
+    let eightyeightXthirtyone = {filename: "sendit.gif", url: "https://example.com"};
+
+    onMount(() => {
+        eightyeightXthirtyone = getRandom88x31();
+    });
+    const currentYear = new Date();
 </script>
 
 <main>
     <div class="footer">
         <p>© 2026-{currentYear.getFullYear()} Delfi-CH</p>
         <p>View the source code <a href="https://github.com/Delfi-CH/delfi-page-new" target="_blank">here</a></p>
+        <div class="eightyeightXthirtyone">
+            <a href={eightyeightXthirtyone.url} target="_blank"><img src={"/88x31/" + eightyeightXthirtyone.filename} alt="88x31"></a>
+        </div>
     </div>
 </main>
 
@@ -15,13 +25,27 @@
         bottom: 0;
         left: 0;
         right: 0;
-        margin-left: auto;
-        margin-right: auto;
-        width: 100%;
-        text-align: center;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: space-between;
+        padding: 0 1rem;
         color: #1A2426;
         background: #34c777;
         border: 5px solid #34c777;
-        max-height: 15%;
+    }
+    @media screen and (min-width: 599px) {
+        .footer {
+            flex-direction: row;
+        }
+    }
+
+    .footer p {
+        margin: 0;
+    }
+
+    .eightyeightXthirtyone {
+        display: flex;
+        align-items: center;
     }
 </style>
