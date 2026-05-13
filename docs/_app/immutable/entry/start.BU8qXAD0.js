f7ae4f4a915463ccb324cfbeeb463a637330f008
Delfi-CH <delfi@delfi.dev>
2026-05-13 09:14:52 +0200

    Continious build

--- docs/_app/immutable/entry/start.BU8qXAD0.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CnoRWTsI.js";import{B as t}from"../chunks/DcUvtX_d.js";export{t as load_css,r as start};
---
+++ docs/_app/immutable/entry/start.Cr_9rL1m.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/gsW1-oTj.js";import{B as t}from"../chunks/C-I3QcJ_.js";export{t as load_css,r as start};
