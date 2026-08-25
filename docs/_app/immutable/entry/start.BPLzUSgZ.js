453e9d95555f39cc67542610d7172154e50fd0c0
Delfi-CH <delfi@delfi.dev>
2026-08-25 12:51:14 +0200

    Continious build

---
+++ docs/_app/immutable/entry/start.BPLzUSgZ.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DUERYwFL.js";import{B as t}from"../chunks/OftgbMrS.js";export{t as load_css,r as start};
--- docs/_app/immutable/entry/start.Cr_9rL1m.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/gsW1-oTj.js";import{B as t}from"../chunks/C-I3QcJ_.js";export{t as load_css,r as start};
